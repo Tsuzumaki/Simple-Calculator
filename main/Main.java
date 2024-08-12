79f7bcaea78298b9449535a3ba2e5d106bf9f854
Tsuzumaki <nicolasresurreccion@gmail.com>
2024-08-12 18:45:41 +0800

    Frame
    added Frame in Main

---
+++ main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+
+        frame.setTitle("Simple Calculator");
+        frame.setSize(700,700);
+
+    }
+}
